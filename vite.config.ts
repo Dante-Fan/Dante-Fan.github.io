4f972c26ee9d10964c30ac7ec9618887ed57f803
Will Davidson <will.davidson+git@audinate.com>
2025-12-08 15:17:50 -0800

    Updated base line to /

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // IMPORTANT: If you rename the repository, change the base path below to '/<your-repo-name>/'
 export default defineConfig({
   plugins: [react()],
-  base: '/Dante-Fan.github.io/',
+  base: '/',
 })
